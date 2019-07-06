7001b687a9660f9afa273c1c11b50144ae637426
John DeFalco <jdefalco@gmail.com>
2019-07-06 07:25:48 -0400

    Add project files.

---
+++ EmbeddedRavenDB/Program.cs
@@ -0,0 +1,46 @@
+﻿using Raven.Client.ServerWide;
+using Raven.Embedded;
+using System;
+using System.Threading.Tasks;
+
+namespace EmbeddedRavenDB
+{
+    class Program
+    {
+        public class User
+        {
+            public string Name;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the user name:");
+            var user = Console.ReadLine();
+
+            EmbeddedServer.Instance.StartServer(new ServerOptions
+            {
+                DataDirectory = "D:\\RavenData",
+                ServerUrl = "http://127.0.0.1:8080"
+            });
+
+            var databaseOptions = new DatabaseOptions(new DatabaseRecord
+            {
+                DatabaseName = "Embedded"
+            });
+
+            using (var store = await EmbeddedServer.Instance.GetDocumentStoreAsync(databaseOptions))
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User { Name = user });
+                    await session.SaveChangesAsync();
+                }
+            }
+
+            EmbeddedServer.Instance.OpenStudioInBrowser();
+
+            Console.WriteLine("Press ENTER to quit this program (and close RavenDB)");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EmbeddedRavenDB472/Program.cs
@@ -0,0 +1,33 @@
+﻿using Raven.Embedded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbeddedRavenDB472
+{
+    class Program
+    {
+        public class User
+        {
+            public string Name;
+        }
+
+        static void Main(string[] args)
+        {
+            EmbeddedServer.Instance.StartServer();
+            var store = EmbeddedServer.Instance.GetDocumentStore("Northwind");
+
+            using (var s = store.OpenSession())
+            {
+                s.Store(new User { Name = "John" });
+                s.SaveChanges();
+            }
+            EmbeddedServer.Instance.OpenStudioInBrowser();
+
+            Console.WriteLine("Press ENTER to quit this program (and close RavenDB)");
+            Console.ReadLine();
+        }
+    }
+}
